0ec2e0d346312de1ab1c2cd03292ab3e6586443d
Ivan <github-ifermon@sneakemail.com>
2017-06-16 07:26:19 -0400

    Fixed logic which was not working.

--- light_timer.py
+++ light_timer.py
@@ -2,6 +2,7 @@
 
 import sdnotify
 from datetime import datetime
+from datetime import timedelta
 from pytz import timezone
 import pigpio
 import time
@@ -12,8 +13,8 @@ WDOG_INTERVAL=15
 # So example, we turn on at 4am and turn off at 10pm
 # Use 24 hour clock 
 # Assume time in local timezone (EST)
-TURN_ON = 4
-TURN_OFF = 22
+TURN_ON = 3
+TURN_OFF = 21
 
 # This is the broadcom pin identifier
 LIGHT_SIGNAL_PIN = 7
@@ -28,7 +29,7 @@ def next_end_time():
     nw = now()
     if nw.hour >= TURN_OFF:
         # Need to increment the day
-        tomorrow = nw + datetime.timedelta(days=1)
+        tomorrow = nw + timedelta(days=1)
         ret_dt = datetime(tomorrow.year, tomorrow.month, tomorrow.day, TURN_OFF, tzinfo=tz())
     else:
         ret_dt = datetime(nw.year, nw.month, nw.day, TURN_OFF, tzinfo=tz())
@@ -36,9 +37,9 @@ def next_end_time():
 
 def next_start_time():
     nw = now()
-    if nw.hour < TURN_ON:
+    if nw.hour >= TURN_ON:
         # need to increment the day
-        tomorrow = nw + datetime.timedelta(days=1)
+        tomorrow = nw + timedelta(days=1)
         ret_dt = datetime(tomorrow.year, tomorrow.month, tomorrow.day, TURN_ON, tzinfo=tz())
     else:
         ret_dt = datetime(nw.year, nw.month, nw.day, TURN_ON, tzinfo=tz())
@@ -69,8 +70,15 @@ if __name__ == "__main__":
     n = sdnotify.SystemdNotifier()
     n.notify("READY=1")
 
+    print("TURN_ON = {}".format(TURN_ON))
+    print("TURN_OFF = {}".format(TURN_OFF))
+
     while True:
-        if now() < next_end_time() and now() > next_start_time():
+        if now().hour < TURN_OFF and now().hour > TURN_ON:
+            print("Turning on lights")
+            print("now = {}".format(now()))
+            print("next_end_time = {}".format(next_end_time()))
+            print("next_start_time = {}".format(next_start_time()))
             turn_on_lights()
 
             # Wake up every few secs to tell systemd we are alive
@@ -79,10 +87,12 @@ if __name__ == "__main__":
                 time.sleep(WDOG_INTERVAL)
                 n.notify("WATCHDOG=1")
         else:
+            print("Turning off lights")
             turn_off_lights()
 
             # Wake up every few secs to tell systemd we are alive
             total_sleep_time = (next_start_time() - now()).seconds
+            print("Total sleep time = {}".format(total_sleep_time))
             for intervals in range(int(total_sleep_time/WDOG_INTERVAL)):
                 time.sleep(WDOG_INTERVAL)
                 n.notify("WATCHDOG=1")
